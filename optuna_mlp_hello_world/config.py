0d8e5456b2014fcdce7b61f4c97d8524f61cd2d5
Kelvin Ducray <kelvinducray@192-168-1-101.tpgi.com.au>
2021-10-30 21:29:13 +1100

    Implemented pydantic BaseSettings

--- optuna_mlp_hello_world/config.py
+++ optuna_mlp_hello_world/config.py
@@ -1,19 +1,22 @@
-class Settings:
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
     # General training settings
-    DATA_DIR = "./data"
-    BATCH_SIZE = 64
-    INPUT_LEN = 28 ** 2  # Unrowed MNIST image
-    NO_OF_CLASSES = 10
+    DATA_DIR: str = "./data"
+    BATCH_SIZE: int = 64
+    INPUT_LEN: int = 28 ** 2  # Unrowed MNIST image
+    NO_OF_CLASSES: int = 10
 
     # Optuna hyperparameter search space
-    NO_OF_LAYERS_MIN = 1
-    NO_OF_LAYERS_MAX = 10
+    NO_OF_LAYERS_MIN: int = 1
+    NO_OF_LAYERS_MAX: int = 10
 
-    HIDDEN_UNITS_MIN = 1
-    HIDDEN_UNITS_MAX = 100
+    HIDDEN_UNITS_MIN: int = 1
+    HIDDEN_UNITS_MAX: int = 100
 
-    LEARNING_RATE_MIN = 1e-5
-    LEARNING_RATE_MAX = 0.2
+    LEARNING_RATE_MIN: float = 1e-5
+    LEARNING_RATE_MAX: float = 0.2
 
-    DROPOUT_RATE_MIN = 0
-    DROPOUT_RATE_MAX = 1
+    DROPOUT_RATE_MIN: int = 0
+    DROPOUT_RATE_MAX: int = 1
